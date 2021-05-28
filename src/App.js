530344366cfac46ea9a1fbe57929e970f7410c5b
=carotsusison <=tnduy.16.06.1998@gmail.com>
2021-05-28 18:47:52 +0700

    change menu on mobile

--- src/App.js
+++ src/App.js
@@ -21,48 +21,30 @@ export default class App extends Component {
         <>
           <ToastContainer />
           <Router>
-            <div>
-              <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <Link to="/" className="navbar-brand">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+              <Link to="/" className="navbar-brand">
                   Dashboard
                 </Link>
-                <button
-                  className="navbar-toggler"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="/#navbarNav"
-                  aria-controls="navbarNav"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span className="navbar-toggler-icon" />
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                  <ul className="navbar-nav">
-                    <li className="nav-item active">
-                      <Link to="/home" className="nav-link">
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                  <Link to="/home" className="nav-link">
                         Home
                       </Link>
-                    </li>
-                    <li className="nav-item">
                       <Link to="/months" className="nav-link">
                         Months
                       </Link>
-                    </li>
-                    <li className="nav-item">
                       <Link to="/weeks" className="nav-link">
                         Weeks
                       </Link>
-                    </li>
-                    <li className="nav-item">
                       <Link to="/days" className="nav-link">
                         Days
                       </Link>
-                    </li>
-                  </ul>
                 </div>
-              </nav>
-            </div>
+              </div>
+            </nav>
             <Switch>
               <Route exact path="/">
                 <Dashboard />
